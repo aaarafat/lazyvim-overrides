68028d390f1141723e92e88cc470d88f6bf6e564
aaarafat <aaarafat@yahoo.com>
2024-03-07 14:21:42 +0200

    update

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -1,12 +1,23 @@
 return {
--- one_monokai
-  { "cpea2506/one_monokai.nvim" },
+    -- one_monokai
+    { "cpea2506/one_monokai.nvim" },
 
-  -- Configure LazyVim to load one_monokai
-  {
-    "LazyVim/LazyVim",
-    opts = {
-      colorscheme = "one_monokai",
-    },
-  }
+    -- -- Configure LazyVim to load one_monokai
+    -- { function()
+    --     if not vim.g.vscode then
+    --         return  {
+    --             "LazyVim/LazyVim",
+    --             opts = {
+    --                 colorscheme = "one_monokai",
+    --             },
+    --         } 
+    --     else
+    --         return  {
+    --             "LazyVim/LazyVim",
+    --             opts = {
+    --                 colorscheme = ""
+    --             }
+    --         } 
+    --     end
+    -- end },
 }
